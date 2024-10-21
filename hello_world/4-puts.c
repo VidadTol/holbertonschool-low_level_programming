7835ad542f2bc2dec29b754212667aafed2d89e1
David Tolza <9769@holbertonstudents.com>
2024-10-21 15:02:53 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Point d'entrée
-* Retour : Toujours 0 (Succès)
+* main - Entry point
+* Retour : Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

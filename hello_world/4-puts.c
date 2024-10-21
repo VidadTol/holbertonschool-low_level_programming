9581aac5930ca90c9fddd3a2837f2dc20c688897
David Tolza <9769@holbertonstudents.com>
2024-10-21 14:24:14 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
-# include <studio.h>
-int main (void)
-{ puts ("\"Programming is like building a multilingual puzzle);
-	return (0);
+# include  <stdio.h>
+
+ /** 
+* main - Point d'entrée 
+* Retour : Toujours 0 (Succès) 
+*/ 
+
+int  main ( void )
+ { 
+ puts ( "\"La programmation, c'est comme construire un puzzle multilingue" ); 
+
+  return ( 0 ); 
 }

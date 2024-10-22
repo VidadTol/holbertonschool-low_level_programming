b57af0372299cda40c4b8d94126bb02e0fe5ab04
David Tolza <9769@holbertonstudents.com>
2024-10-22 16:17:42 +0200

    print alpha bis

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase, and new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	for (letter = 'A' ; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

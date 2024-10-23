c0074733ac9d9d693c75c7c56e5b6a7424ed4613
David Tolza <9769@holbertonstudents.com>
2024-10-23 13:58:34 +0200

    print base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+
+		printf("%x", num);
+
+	printf("\n");
+
+	return (0);
+}

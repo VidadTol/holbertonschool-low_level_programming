b863a9902f5a9f6362603f5ca7724c9d476e0e6c
David Tolza <9769@holbertonstudents.com>
2024-10-24 15:35:51 +0200

    main

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -  that prints _putchar
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

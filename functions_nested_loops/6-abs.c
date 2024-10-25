38be9cbe4480a3ab4b7371339fc3dab3c227aa37
David Tolza <9769@holbertonstudents.com>
2024-10-25 15:47:51 +0200

    absolu

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs - that computes the absolute value of an integer
+*@x: argument
+*Return: 0 Always
+*/
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}

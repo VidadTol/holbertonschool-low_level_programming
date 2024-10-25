c711844b5447c51eee2e5b28007b371f5cf09e47
David Tolza <9769@holbertonstudents.com>
2024-10-25 14:47:45 +0200

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,19 +10,19 @@
 
 int print_sign(int n)
 {
-	if (n > 49)
+	if (n > 48)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 48)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
+	else if (n < 48)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }

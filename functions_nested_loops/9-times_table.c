8a6e1c6a9ccb3a4e9d6a8afe5d3ec67abfcbbef6
David Tolza <9769@holbertonstudents.com>
2024-10-26 14:47:43 +0200

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,24 +12,20 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		_putchar(row + '0');
+		_putchar('0');
 
 		for (col = 1; col <= 9; col++)
 		{
-			prod = col * row;
 			_putchar(',');
 			_putchar(' ');
 
+			prod = col * row;
+
 			if (prod <= 9)
-			{
-			_putchar(' ');
-			_putchar(prod + '0');
-			}
+				_putchar(' ');
 			else
-			{
-			_putchar((prod / 10) + '0');
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}

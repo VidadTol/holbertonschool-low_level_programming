2299a8121d279396f1c52b437ca52bf283d1f664
David Tolza <9769@holbertonstudents.com>
2024-10-28 13:47:28 +0100

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -  that multiplies two integers
+ * Return: first number
+ * Return: second number
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

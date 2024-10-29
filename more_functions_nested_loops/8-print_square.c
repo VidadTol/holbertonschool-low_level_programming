e91f21f0ca328d0ed3b979124c89517d707a0ac7
David Tolza <9769@holbertonstudents.com>
2024-10-29 15:39:42 +0100

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - that prints a square
+ * @size: argument
+ * Return: 0 Always
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

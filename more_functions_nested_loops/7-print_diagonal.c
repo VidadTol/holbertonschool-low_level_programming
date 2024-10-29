8a2548fd86a08f9ef486f08fc53e19053b503a6f
David Tolza <9769@holbertonstudents.com>
2024-10-29 12:04:49 +0100

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,13 +10,12 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	if (x <= n)
+	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
 	else
-	{	
+	{
 		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y <= x; y++)

b12cb9df8e831c026d44d927a256de9816cccac5
David Tolza <9769@holbertonstudents.com>
2024-11-01 11:53:47 +0100

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to print
+ * return: always 0
+ */
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+			_putchar(str[x]);
+	}
+	_putchar('\n');
+}

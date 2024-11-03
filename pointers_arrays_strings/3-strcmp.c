34082322fc3c329b8ab84caa2a22f132ac5328ab
David Tolza <9769@holbertonstudents.com>
2024-11-03 16:23:09 +0100

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,15 +3,22 @@
  *_strcmp - compare two strings
  *@s1: first string to compare
  *@s2: second string to compare
- *Retrun: 0 always
+ *Return: 0 always
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	if (*s1 == *s2)
+	{
+	return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }

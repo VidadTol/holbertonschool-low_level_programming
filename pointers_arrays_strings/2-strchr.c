839f88d85a6f509d2bcccb510982247356754a33
David Tolza <9769@holbertonstudents.com>
2024-11-04 15:34:05 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	for (; *s != '\0'; s++)
-	{	
+	{
 		if (*s == c)
 		{
 			return (s);
@@ -18,7 +18,7 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 	{
-		return (s);	
+		return (s);
 	}
-		return (NULL); 
+		return (NULL);
 }

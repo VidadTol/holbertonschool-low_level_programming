c02cae400b3df8513f4229a8ab43beb99a38211c
David Tolza <9769@holbertonstudents.com>
2024-11-05 10:28:06 +0100

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strspn - that gets the length of a prefix substring
+ * @s: char
+ * @accept: pointer
+ * return: the number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int a;
+	int k;
+
+	for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+	{
+		for (a = 0; accept[a] != '\0'; a++)
+		{
+			if (s[i] == accept[a])
+			{
+				k++;
+			}
+		}
+	}
+	return (k);
+}

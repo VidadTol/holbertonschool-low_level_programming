3dd9ed0cd90d1306fe669b3cee983d8c689e14a9
David Tolza <9769@holbertonstudents.com>
2024-11-22 14:17:02 +0100

    variadic.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+
+
+
+/**
+ *
+ *
+ *
+ *
+ */
+typedef struct what_format
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+
+
+
 
 int _putchar(char c);
 
@@ -12,5 +29,12 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif

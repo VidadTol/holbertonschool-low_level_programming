867cf1b9a15430f3ce99c48b2fdc602057378461
David Tolza <9769@holbertonstudents.com>
2024-11-22 16:32:29 +0100

    print

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,25 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-
-
 
 /**
- *
- *
- *
- *
+ * struct format_types - defines representing a data type
+ * @f: the function pointer 
+ * @separator: the conversion specifier
  */
+
 typedef struct what_format
 {
-	char *symbol;
-	void (*print)(va_list arg);
-} printer_t;
-
-
-
+	char *type;
+	void (*f)(char *separator, va_list args);
+}what_format;
 
 
 int _putchar(char c);
@@ -32,9 +26,9 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
-void print_char(va_list args);
-void print_int(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
+void print_char(char *separator, va_list args);
+void print_integer(char *separator, va_list args);
+void print_float(char *separator, va_list args);
+void print_char_ptr(char *separator, va_list args);
 
 #endif

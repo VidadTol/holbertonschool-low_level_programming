e374de12e65d67c957067d1817786fb01517fded
David Tolza <9769@holbertonstudents.com>
2024-12-03 10:25:31 +0100

    list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - that returns the number of elements in a linked list_t list
+ * @h: pointer to the first node in the list
+ * Return: the number of nodes in the list
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

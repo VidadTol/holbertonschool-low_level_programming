3e620628013aa5ee129769477b836f96ccf5379d
David Tolza <9769@holbertonstudents.com>
2024-12-03 18:13:50 +0100

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,25 +2,26 @@
 
 /**
  * print_list - function that prints all the elements of a list_t
- * @h: pointer to the first node in the list
+ * @h: pointer to the beginning of the list
  * Return: the number of nodes in the list
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t count = 0;/*utilisation de size_t pour le compteur*/
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-
-			printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[%d] %s\n", h->len, h->str);/*utilisation de %u pour usigned int*/
+		}
 
 		h = h->next;
-
 		count++;
 	}
 

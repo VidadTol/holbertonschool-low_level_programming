aebf286e5b2b19fea86cf530fc7a3beceabb4aaf
David Tolza <9769@holbertonstudents.com>
2024-12-03 15:08:12 +0100

    add node end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_node_end - that adds a new node at the end of a list_t list
+ * @head: pointer to the pointer of the head of the list
+ * @str: string to duplicate and add to the new node
+ * Return: adress of the new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = NULL;
+
+	new_node = malloc(sizeof(list_t));/*alloue la memoire nouveau noeud*/
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);/*duplique la chaine str*/
+	if (new_node->str == NULL)
+	{
+		free(new_node);/*libere memoire si strdup échoue*/
+		return (NULL);
+	}
+	new_node->len = strlen(str);/*calcul long de str*/
+	new_node->next = NULL;/* le nouv noeud sera le dernier donc next est NULL*/
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *temp = *head;/*parcourt la liste -> la fin*/
+
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;/* ajoute le nouveau noeud a la fin de la liste*/
+	}
+	return (new_node);/*retourne le nouveau noeud*/
+}

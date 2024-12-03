69c7505bef312036f45c7a2ae9dc0950f5e2a64c
David Tolza <9769@holbertonstudents.com>
2024-12-03 17:17:32 +0100

    modif doublons

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LIST_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,11 +20,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 size_t list_len(const list_t *h);
 

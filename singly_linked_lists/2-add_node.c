175b4293bc6ff26b70ed931dfc78dd7960a4bbb7
David Tolza <9769@holbertonstudents.com>
2024-12-05 11:13:14 +0100

    supp strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,19 +10,29 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node  = NULL; /*initialisation du nouveau noeud*/
+	unsigned int len = 0;
+	
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));/*alloc.. memoire pour le nouveau noeud*/
 	if (new_node == NULL)
 	{
-		free(new_node = NULL);
-		new_node = NULL;
+		free(new_node);
 		return (NULL);/* retourne nul si l'allocation échoue*/
 	}
 	/* duplication de la chaine de str*/
 	/* et affecte le pointeur str du nouv noeud a cette copie*/
 	new_node->str = strdup(str);
-	/* calcul la long de str et l'affecte au membre len du nouv noeud*/
-	new_node->len = strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = len;
 	/* mise a jour des pointeurs*/
 	/* le pointeur next du nouv noeud pointe vers l'actuelle tete de liste *head*/
 	new_node->next = *head;

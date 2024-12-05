c31ec2c5e95acbff5f1af4bd377dc38c6ed0616f
David Tolza <9769@holbertonstudents.com>
2024-12-05 15:36:37 +0100

    insert dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *prev_node = *h;
 
-	if(idx == 0)
+	if (idx == 0)
 	{
-		return(add_dnodeint(h,n));
+		return (add_dnodeint(h, n));
 	}
 	while (i < (idx - 1))
 	{
@@ -24,17 +24,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
-		prev_node = prev_node ->next;
+		prev_node = prev_node->next;
 		i++;
 	}
-	if (prev_node ->next == NULL)
+	if (prev_node->next == NULL)
 	{
-		return (add_dnodeint_end(h,n));
+		return (add_dnodeint_end(h, n));
 	}
 		new_node->n = n;
-		new_node ->next = prev_node ->next;
+		new_node->next = prev_node->next;
 		new_node->prev = prev_node;
-		prev_node->next->prev=new_node;
+		prev_node->next->prev = new_node;
 		prev_node->next = new_node;
 
 		return (new_node);

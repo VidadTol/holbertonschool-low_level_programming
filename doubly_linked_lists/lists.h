9646efdd598ed5e65d554f959c40fca870529257
David Tolza <9769@holbertonstudents.com>
2024-12-05 13:22:35 +0100

    correction betty

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -13,14 +13,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
  */
 
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -36,5 +35,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 int sum_dlistint(dlistint_t *head);
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif
